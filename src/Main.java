ea7a8a4896c0e72e652e3c08a7dfc2d79314898e
Mauricio <mauri.f.mores@gmail.com>
2024-04-27 23:21:58 -0300

    REFACTOR: finish case one and two

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import DTOs.MultiCurrencyConversionDTO;
+import DTOs.TypeCurrencyDTO;
 import enums.TypeCurrency;
 import models.Conversion;
 import services.HttpClientService;
@@ -21,6 +23,9 @@ public class Main {
         //instancia de lectura de entrada
         Scanner scn = new Scanner(System.in);
 
+        //traigo la lista de tipos de cambio
+        TypeCurrencyDTO typeCurrencyDTO = httpClientService.getTypeCurrencies();
+
         //creacion de un menu de usuario
         System.out.println("******************** Conversion de Monedas ********************\n");
         Util.mainMenu();
@@ -29,14 +34,37 @@ public class Main {
         boolean exit = false;
 
         do {
-            byte numSelection = scn.nextByte();
+            byte numSelection = Util.validationByte((byte) 5, (byte) 1);
             switch (numSelection){
                 case 1:{
-
+                    TypeCurrency typeCurrency = Util.controlCurrencyType("**** Ingrese un tipo de moneda el cual quiera ver conversiones respecto a esta ****");
+                    httpClientService.getMultipleConversion(typeCurrency);
+                    Util.mainMenu();
                     break;
                 }
                 case 2:{
-
+                    if (typeCurrencyDTO != null){
+                        System.out.println("""
+                        Seleccione una opción de búsqueda o impresión total:
+                        1_ Búsqueda por ingreso de palabra clave
+                        2_ impresión de todos los tipos de cambio
+                        """);
+                        byte select = Util.validationByte((byte) 2, (byte) 1);
+                        switch (select){
+                            case 1:{
+                                Util.filterCurrencyType(typeCurrencyDTO);
+                                break;
+                            }
+                            case 2:{
+                                Util.printingCurrencyTypes(typeCurrencyDTO.supported_codes()," Aquí podrá ver los tipos de cambio: \n Código ISO ------ Nombre de Moneda ");
+                                break;
+                            }
+                        }
+                    }
+                    else{
+                        System.out.println("A ocurrido un error vuelva a intentarlo");
+                    }
+                    Util.mainMenu();
                     break;
                 }
                 case 3:{
@@ -45,17 +73,17 @@ public class Main {
                     TypeCurrency baseTypeCurrency = Util.controlCurrencyType(" ** Ingrese el tipo de cambio de este monto " + amount + "\n ** Ejemplo: USD - ARS - EUR");
                     TypeCurrency targetTypeCurrency = Util.controlCurrencyType("Por ultimo ingrese el tipo de cambio al cual desea convertir");
                     httpClientService.getConversionTwoCurrency(baseTypeCurrency, targetTypeCurrency, amount);
-                    System.out.println("El resultado de la conversion de:\n " + conversion.getAmountEntered() + " " + baseTypeCurrency.name() + " es de " + conversion.getAmountResult() + " " + targetTypeCurrency.name());
-                    System.out.println("*** Si desea puede ver sus ultimas conversiones en el historial ***\n");
-
                     Util.mainMenu();
                     break;
                 }
                 case 4:{
+                    System.out.println(" ****** Su historial de conversiones ****** ");
+
+                    Util.mainMenu();
                     break;
                 }
                 case 5:{
-                    System.out.println("************ Gracias por utilizar nuestro conversor de moneda");
+                    System.out.println("************ Gracias por utilizar nuestro conversor de moneda ************");
                     exit = true;
                     break;
                 }
