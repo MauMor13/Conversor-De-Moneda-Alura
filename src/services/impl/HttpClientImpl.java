3448ee847b5adb9a2b4b7c45c9588fecf1bc29b4
Mauricio <mauri.f.mores@gmail.com>
2024-04-27 23:27:30 -0300

    REFACTOR: change in methods

--- src/services/impl/HttpClientImpl.java
+++ src/services/impl/HttpClientImpl.java
@@ -1,10 +1,12 @@
 package services.impl;
 import DTOs.ConversionPairDTO;
-import DTOs.CurrencyDTO;
+import DTOs.MultiCurrencyConversionDTO;
 import DTOs.TypeCurrencyDTO;
 import enums.TypeCurrency;
+import models.Conversion;
 import services.HttpClientService;
 import services.JsonTransformService;
+import utilitis.Util;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -22,8 +24,8 @@ public class HttpClientImpl implements HttpClientService {
         //ruta completa
         String urlRequest = partialUrl.concat(keyRequest.concat(endUrl));
 
-        //instancia de cliente para realizar la peticion
-        HttpClient client =HttpClient.newHttpClient();
+        //instancia de cliente para realizar la petición
+        HttpClient client = HttpClient.newHttpClient();
         try{
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(URI.create(urlRequest))
@@ -42,14 +44,25 @@ public class HttpClientImpl implements HttpClientService {
     public TypeCurrencyDTO getTypeCurrencies() {
         String endUrl = "/codes";
         String json = this.requestStructure(endUrl);
-        return this.jsonTransform.deserializationGson(json, TypeCurrencyDTO.class);
+        if (json != null){
+            return this.jsonTransform.deserializationGson(json, TypeCurrencyDTO.class);
+        }
+        return null;
     }
 
     @Override
-    public CurrencyDTO getCurrencyResponse(String typeCurrency) {
-        String endUrl = "/latest/USD";
+    public void getMultipleConversion(TypeCurrency typeCurrency) {
+        String endUrl = "/latest/".concat(typeCurrency.name());
         String json = this.requestStructure(endUrl);
-        return this.jsonTransform.deserializationGson(json, CurrencyDTO.class);
+        if (json != null){
+            MultiCurrencyConversionDTO multiCurrencyConversionDTO = this.jsonTransform.deserializationGson(json, MultiCurrencyConversionDTO.class);
+            if (multiCurrencyConversionDTO != null){
+                Util.printingCurrencyTypes(multiCurrencyConversionDTO.conversion_rates(),"Aquí podrá ver la conversion de las monedas respecto a un " + multiCurrencyConversionDTO.base_code());
+            }
+        }
+        else {
+            System.out.println("A ocurrido un error vuelva a intentarlo");
+        }
     }
 
     @Override
@@ -57,10 +70,15 @@ public class HttpClientImpl implements HttpClientService {
         String endUrl = "/pair/".concat(baseType.name().concat("/".concat(targetType.name().concat("/".concat(String.valueOf(amount))))));
         String json = this.requestStructure(endUrl);
         if (json != null){
-            this.jsonTransform.deserializationGson(json, ConversionPairDTO.class);
+            ConversionPairDTO conversionPairDTO = this.jsonTransform.deserializationGson(json, ConversionPairDTO.class);
+            if (conversionPairDTO != null){
+                Conversion conversion = new Conversion(conversionPairDTO, amount);//*************** Falta guardar la conversion en un array list **************
+                System.out.println("El resultado de la conversion de:\n " + conversion.getAmountEntered() + " " + baseType.name() + " es de " + conversion.getAmountResult() + " " + targetType.name());
+                System.out.println("*** Si desea puede ver sus ultimas conversiones en el historial ***\n");
+            }
         }
         else {
-            System.out.println("A ocurrido un error vuelva a intentarlo ");
+            System.out.println("A ocurrido un error vuelva a intentarlo");
         }
     }
 }
